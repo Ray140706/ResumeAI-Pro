eaa1429f503a34c8769e0ba52f8c6f3c112274eb
Ray140706 <76245574+Ray140706@users.noreply.github.com>
2026-04-02 08:42:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ if st.button("Analyze"):
 
             # Preview
             st.subheader("📄 Resume Preview")
-            st.write(resume_text[:399])
+            st.write(resume_text[:391])
 
             st.subheader("📄 Job Description Preview")
             st.write(jd_text[:399])
